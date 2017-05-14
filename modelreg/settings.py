9aa56879d099abcefc8e7b25e3d5e3502e6d4f92
David Vogt <david.vogt@adfinis-sygroup.ch>
2017-05-14 19:10:26 +0200

    Settings: context processors, so we get access to the request.
    Also, add redirect URL so we have somewhere to go after loggin gin

--- modelreg/settings.py
+++ modelreg/settings.py
@@ -42,6 +42,20 @@ INSTALLED_APPS = [
 ]
 
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+
+    "django.core.context_processors.request",
+)
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -129,3 +143,5 @@ STATICFILES_DIRS = [
 ACCOUNT_ACTIVATION_DAYS = 7
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = 'profile'
