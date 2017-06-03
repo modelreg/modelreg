6f26c7e302ebc4673248d38156ab21eb3084797c
Marc Urben <aegnor@mittelerde.ch>
2017-06-03 12:22:59 +0200

    Removed the branch check again. Still not working.

--- modelreg/views.py
+++ modelreg/views.py
@@ -182,10 +182,10 @@ def system_update(req):
     from django.core.management import call_command
 
     # This does not work properly yet, so disabling the code for now
-    if req.POST['ref'] != 'refs/heads/master':
-        response = HttpResponseBadRequest()
-        response.write("Not master branch, not updating")
-        return response
+    # if req.POST['ref'] != 'refs/heads/master':
+    #     response = HttpResponseBadRequest()
+    #     response.write("Not master branch, not updating")
+    #     return response
 
 
     git_cmd = git.cmd.Git(settings.BASE_DIR)
